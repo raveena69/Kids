2884f8fc1b7a231940da4fbf378bc1e7e0155f5f
pansal96 <sacheewa96@gmail.lk>
2020-09-27 14:50:36 +0530

    Add new files in project.

--- app/src/main/java/com/example/kids/Mchecking.java
+++ app/src/main/java/com/example/kids/Mchecking.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SColours extends AppCompatActivity {
+public class Mchecking extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_s_colours);
+        setContentView(R.layout.activity_mchecking);
     }
 }
