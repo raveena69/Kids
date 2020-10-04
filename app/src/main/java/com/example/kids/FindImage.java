95489457434dccf09d4e64a1e08d7bd58b955029
mahesh pc <madhuchamara26@gmail.com>
2020-10-04 19:57:16 +0530

    added new files

---
+++ app/src/main/java/com/example/kids/FindImage.java
@@ -0,0 +1,17 @@
+package com.example.kids;
+
+public class FindImage {
+    private String name;
+
+    public FindImage() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
